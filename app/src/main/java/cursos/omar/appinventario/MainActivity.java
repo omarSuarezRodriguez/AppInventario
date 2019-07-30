908a0c74b710963a784b5f1e2422da2fdc156d73
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-07-30 01:26:12 -0500

    Commit

--- app/src/main/java/cursos/omar/appinventario/MainActivity.java
+++ app/src/main/java/cursos/omar/appinventario/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); //gg
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
