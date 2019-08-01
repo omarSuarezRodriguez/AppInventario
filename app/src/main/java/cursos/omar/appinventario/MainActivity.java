372c17473bc4146f18564e8e553f02244a00d069
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-08-01 01:16:26 -0500

    Commit

--- app/src/main/java/cursos/omar/appinventario/MainActivity.java
+++ app/src/main/java/cursos/omar/appinventario/MainActivity.java
@@ -6,13 +6,29 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+//    @BindView(R.id.fragment_container)
+//    FrameLayout fragmentContainer;
+//    @BindView(R.id.bottom_navigation)
+//    BottomNavigationView bottomNavigation;
+//    @BindView(R.id.container_main)
+//    RelativeLayout containerMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setContentView(R.layout.item_catalogue);
+
+        ButterKnife.bind(this);
+
 
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
@@ -58,3 +74,111 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
+
+//    <?xml version="1.0" encoding="utf-8"?>
+//<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+//        xmlns:app="http://schemas.android.com/apk/res-auto"
+//        xmlns:tools="http://schemas.android.com/tools"
+//        android:layout_width="match_parent"
+//        android:layout_height="match_parent"
+//        android:background="@android:color/holo_red_light"
+//        tools:context=".MainActivity">
+//
+//
+//
+//
+//
+//<FrameLayout
+//        android:id="@+id/fragment_container"
+//                android:layout_width="match_parent"
+//                android:layout_height="match_parent"
+//                android:layout_above="@+id/bottom_navigation"
+//
+//
+//                />
+//
+//<android.support.design.widget.BottomNavigationView
+//        android:id="@+id/bottom_navigation"
+//        android:layout_width="match_parent"
+//        android:layout_height="wrap_content"
+//        android:layout_alignParentBottom="true"
+//        android:background="?android:attr/windowBackground"
+//        app:labelVisibilityMode="labeled"
+//        app:menu="@menu/bottom_navigation" />
+//
+//
+//<!--
+//        Para cambiar el color del icono y del texto del bottom_navigation
+//        app:itemIconTint="@android:color/holo_red_light"
+//        app:itemTextColor="@android:color/holo_red_light"
+//        -->
+//
+//<!--
+//        Para mostrar siempre los labels (texto) de los bottom_navigation
+//        app:labelVisibilityMode="labeled"
+//        -->
+//
+//
+//</RelativeLayout>
+
+
+//<FrameLayout
+//        android:id="@+id/fragment_container"
+//                android:layout_width="match_parent"
+//                android:layout_height="match_parent"
+//                android:layout_above="@+id/bottom_navigation"
+//
+//
+//
+//
+//
+//                />
+
+
+//Segunda Forma:
+
+
+//    <?xml version="1.0" encoding="utf-8"?>
+//<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+//        xmlns:app="http://schemas.android.com/apk/res-auto"
+//        xmlns:tools="http://schemas.android.com/tools"
+//        android:layout_width="match_parent"
+//        android:layout_height="match_parent"
+//        android:background="@android:color/holo_red_light"
+//        tools:context=".MainActivity">
+//
+//
+//<android.support.v7.widget.LinearLayoutCompat
+//        android:id="@+id/fragment_container"
+//        android:layout_width="match_parent"
+//        android:layout_height="match_parent"
+//        android:layout_above="@+id/bottom_navigation">
+//
+//
+//</android.support.v7.widget.LinearLayoutCompat>
+//
+//
+//<android.support.design.widget.BottomNavigationView
+//        android:id="@+id/bottom_navigation"
+//        android:layout_width="match_parent"
+//        android:layout_height="wrap_content"
+//        android:layout_alignParentBottom="true"
+//        android:background="?android:attr/windowBackground"
+//        app:labelVisibilityMode="labeled"
+//        app:menu="@menu/bottom_navigation" />
+//
+//
+//<!--
+//        Para cambiar el color del icono y del texto del bottom_navigation
+//        app:itemIconTint="@android:color/holo_red_light"
+//        app:itemTextColor="@android:color/holo_red_light"
+//        -->
+//
+//<!--
+//        Para mostrar siempre los labels (texto) de los bottom_navigation
+//        app:labelVisibilityMode="labeled"
+//        -->
+//
+//
+//</RelativeLayout>
--- app/src/main/java/cursos/omar/appinventario/Product.java
+++ app/src/main/java/cursos/omar/appinventario/Product.java
@@ -1,5 +1,7 @@
 package cursos.omar.appinventario;
 
+import java.util.Objects;
+
 public class Product {
 
 
@@ -8,14 +10,107 @@ public class Product {
     private long barCode;       //Codigo de barras
     private int hourEntry;      //Hora de entrada
     private int hourOutput;      //Hora de salida
-
-    private String image;       //Imagen del producto
-
-
+    private String notes;       //Si queremos ponerle alguna anotacion
+    private int order;          //Si le queremos agregar algun orden
 
 
+    private String photoUrl;       //Imagen del producto ubicacion
 
-//    Calendar rightNow = Calendar.getInstance();
+
+    public Product() {
+    }
+
+    public Product(long id, String name, long barCode, int hourEntry, int hourOutput,
+                   String notes, int order, String photoUrl) {
+        this.id = id;
+        this.name = name;
+        this.barCode = barCode;
+        this.hourEntry = hourEntry;
+        this.hourOutput = hourOutput;
+        this.notes = notes;
+        this.order = order;
+        this.photoUrl = photoUrl;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getBarCode() {
+        return barCode;
+    }
+
+    public void setBarCode(long barCode) {
+        this.barCode = barCode;
+    }
+
+    public int getHourEntry() {
+        return hourEntry;
+    }
+
+    public void setHourEntry(int hourEntry) {
+        this.hourEntry = hourEntry;
+    }
+
+    public int getHourOutput() {
+        return hourOutput;
+    }
+
+    public void setHourOutput(int hourOutput) {
+        this.hourOutput = hourOutput;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+    //    Calendar rightNow = Calendar.getInstance();
 //    int currentHourIn24Format = rightNow.get(Calendar.HOUR_OF_DAY); // return the hour in 24 hrs format (ranging from 0-23)
 //
 //    int currentHourIn12Format = rightNow.get(Calendar.HOUR); // return the hour in 12 hrs format (ranging from 0-11)
