c65fde91a6007bc9c66b3e2e61bf6c719b83f838
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-07-31 19:17:32 -0500

    Commit

---
+++ app/src/main/java/cursos/omar/appinventario/Product.java
@@ -0,0 +1,24 @@
+package cursos.omar.appinventario;
+
+public class Product {
+
+
+    private long id;        //Idenfiticador
+    private String name;        //Nombre
+    private long barCode;       //Codigo de barras
+    private int hourEntry;      //Hora de entrada
+    private int hourOutput;      //Hora de salida
+
+    private String image;       //Imagen del producto
+
+
+
+
+
+//    Calendar rightNow = Calendar.getInstance();
+//    int currentHourIn24Format = rightNow.get(Calendar.HOUR_OF_DAY); // return the hour in 24 hrs format (ranging from 0-23)
+//
+//    int currentHourIn12Format = rightNow.get(Calendar.HOUR); // return the hour in 12 hrs format (ranging from 0-11)
+
+
+}
